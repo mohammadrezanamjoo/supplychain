31cd481f628bf287c58eee0c76dd7f96d212ed14
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-07-14 18:05:26 +0330

    Update supplychain_v2.sol

--- supplychain_v2.sol
+++ supplychain_v2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract SupplyChain {
-    // Enum representing the various states of a product in the 
+    // Enum representing the various 
     enum State { Created, Manufactured, ForSale, Sold, Shipped, Received }
 
     // Structure representing a Product
